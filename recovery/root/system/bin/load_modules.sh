486e8f5a314fac98d0a1fd5d2bcc48f107085fd7
uotlaf <uotlaf@protonmail.com>
2021-06-29 19:53:26 -0300

    Load touchscreen modules via script

---
+++ recovery/root/system/bin/load_modules.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# Install touchscreen modules
+insmod /vendor/lib/modules/nova_0flash_mmi.ko
+insmod /vendor/lib/modules/himax_v2_mmi_hx83102d.ko
+insmod /vendor/lib/modules/himax_v2_mmi.ko
+
+# Update novatek touchscreen firmware early
+cat /proc/nvt_update
+
+exit 0
